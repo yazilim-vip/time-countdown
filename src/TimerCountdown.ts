f7bf15cfd622e09ed3ed89db9b9f1bbf0e46031e
Azaraks-AG <ahmetgurdal32@gmail.com>
2020-09-03 23:04:16 +0300

    fixed:

---
+++ src/TimerCountdown.ts
@@ -0,0 +1,101 @@
+import { StatusBarItem } from 'vscode';
+import * as vscode from 'vscode';
+//import {Timeout} from 'NodeJs';
+
+const REGEX = /(^\d+h([0-9]|[1-5][0-9])m\s([0-9]|[1-5][0-9])s$|^\d+h ([0-9]|[1-5][0-9])m$|^\d+h ([0-9]|[1-5][0-9])s$|^\dm\s([0-9]|[1-5][0-9])+s$|^\dh$|^\dm$|^\d+s$)/;
+
+export class TimerCountdown {
+
+	myStatusBarItem: StatusBarItem;
+	timer?: NodeJS.Timeout = undefined;
+	timerFlag: boolean = false;
+	timeLeft?: number = undefined;
+
+
+	constructor(myStatusBarItem: StatusBarItem) {
+		this.myStatusBarItem = myStatusBarItem;
+	}
+
+	toggleTimer() {
+		if (this.timerFlag) {
+			// If timer is already started before,  just stop existing timer nothing else matter.
+			this.stopTimer();
+		} else {
+			this.startTimer();
+		}
+	}
+
+	startTimer() {
+		let timerCountdown : TimerCountdown = this;
+		vscode.window.showInputBox({
+			placeHolder: "Time Amount to Wait"
+		}).then(function(data) {userInputHandler(timerCountdown, data)});
+	}
+
+	stopTimer() {
+		if (this.timer === undefined) {
+			return;
+		}
+		clearInterval(this.timer);
+		this.timerFlag = false;
+		this.myStatusBarItem.text = `Start Time`;
+	}
+}
+
+function userInputHandler(timerCountdown: TimerCountdown, data?: string) {
+	if (data === undefined) {
+		vscode.window.showInformationMessage('Time Format Undefined');
+		return;
+	}
+
+	// Match format samples:
+	// Xh Ym Zs -> X hours, Y minutes Z seconds waiting time
+	// If pattern does not match, returns null
+	let match = data.match(REGEX);
+	if (match === null) {
+		vscode.window.showInformationMessage(`Time Format Undefined ${data}`);
+		return;
+	}
+
+	timerCountdown.timeLeft =getTotalTimeInSec(data);
+	if (timerCountdown.timerFlag) {
+		timerCountdown.stopTimer();
+	}
+
+	timerCountdown.timer = setInterval( function(){countDownLoop(timerCountdown);}, 1000);
+	timerCountdown.timerFlag = true;
+
+};
+
+// Converting user input to second in time.
+function getTotalTimeInSec(userInput: string): number {
+	let totalTimeInSec: number = 0;
+
+	var slices: string[] = userInput.split(' ');
+	slices.forEach(function (slice) {
+		if (userInput.indexOf('h') !== -1) {
+			let hour: number = parseInt(slice.split('h')[0]);
+			totalTimeInSec = totalTimeInSec + (hour * 60 * 60);
+		} else if (userInput.indexOf('m') !== -1) {
+			let minute: number = parseInt(slice.split('m')[0]);
+			totalTimeInSec = totalTimeInSec + (minute * 60);
+		} else if (userInput.indexOf('s') !== -1) {
+			let second: number = parseInt(slice.split('s')[0]);
+			totalTimeInSec = totalTimeInSec + (second);
+		}
+	});
+	return totalTimeInSec;
+}
+
+// Update user notifications when triggered.
+function countDownLoop(timerCountdown : TimerCountdown) {
+
+	if (timerCountdown.timeLeft === undefined || timerCountdown.timeLeft <= 0) {
+		vscode.window.showInformationMessage(`Hands Up!!!`);
+		timerCountdown.stopTimer();
+	} else {
+		timerCountdown.myStatusBarItem.text = `Time Left (${timerCountdown.timeLeft})`;
+		timerCountdown.timeLeft -= 1;
+	}
+
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { TimerCountdown } from './TimerCountdown';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,17 +20,19 @@ export function activate(context: vscode.ExtensionContext) {
 	myStatusBarItem.text = `Start Timer`;
 	myStatusBarItem.show();
 
+	let timerCountdown : TimerCountdown = new TimerCountdown(myStatusBarItem);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let startTimerDisposable = vscode.commands.registerCommand('time-countdown.startTimer', () => {
 		// The code you place here will be executed every time your command is executed
-		
+		timerCountdown.toggleTimer();
 	});
 
 	let stopTimerDisposable = vscode.commands.registerCommand('time-countdown.stopTimer', () => {
 
+		timerCountdown.stopTimer();
 
 	});
 
